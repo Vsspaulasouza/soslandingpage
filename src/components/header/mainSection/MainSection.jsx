d077f879c758296c764be5f4e112e800960421d8
Vinícius Soares <vsspaulasouza@gmail.com>
2022-04-06 01:23:14 -0300

    fix: refactoring scroll effect

--- src/components/header/mainSection/MainSection.jsx
+++ src/components/header/mainSection/MainSection.jsx
@@ -8,10 +8,10 @@ function MainSection() {
   return (
     <section className={Styles.main}>
       <Title>Survive at all costs</Title>
-      <Subtitle style={{ "margin-top": "10px" }}>
+      <Subtitle style={{ marginTop: "10px" }}>
         Experience new social battle royale game
       </Subtitle>
-      <Button href="#Buy-now" style={{ "margin-top": "35px" }}>
+      <Button href="#Buy-now" style={{ marginTop: "35px" }}>
         <span>Buy now on Steam</span>
         <div className={Styles.button__divider} />
         <span>$14.99</span>
--- src/components/header/menu/Menu.jsx
+++ src/components/header/menu/Menu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import debounce from "lodash.debounce";
 import MenuList from "./MenuList";
 import SecondaryMenu from "./SecondaryMenu";
 import MenuMobile from "./MenuMobile";
@@ -8,6 +9,17 @@ import Styles from "./Menu.module.scss";
 function Menu() {
   const [menuActive, setMenuActive] = React.useState("");
   const [scrollClass, setScrollClass] = React.useState("");
+  const ref = React.useRef();
+
+  function handleScroll() {
+    if (window.scrollY === 0) setScrollClass("");
+    else setScrollClass(` ${Styles["header__menu--scroll"]}`);
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", debounce(handleScroll, 20));
+    return window.removeEventListener("scroll", debounce(handleScroll, 20));
+  }, []);
 
   const resolveClass = React.useCallback(
     (compStyle, currentClass, modifier) =>
@@ -17,30 +29,8 @@ function Menu() {
     [menuActive]
   );
 
-  // function debounce(func, wait, immediate) {
-  //   let timeout;
-  //   return (...args) => {
-  //     const context = this;
-  //     const later = () => {
-  //       timeout = null;
-  //       if (!immediate) func.apply(context, args);
-  //     };
-  //     const callNow = immediate && !timeout;
-  //     clearTimeout(timeout);
-  //     timeout = setTimeout(later, wait);
-  //     if (callNow) func.apply(context, args);
-  //   };
-  // }
-
-  function handleScroll() {
-    if (window.scrollY === 0) setScrollClass("");
-    else setScrollClass(` ${Styles["header__menu--scroll"]}`);
-  }
-
-  window.addEventListener("scroll", handleScroll);
-
   return (
-    <nav className={Styles.header__menu + scrollClass}>
+    <nav className={Styles.header__menu + scrollClass} ref={ref}>
       <a href="#home">
         <img src={Logo} alt="Logo" className={Styles.header__logo} />
       </a>
