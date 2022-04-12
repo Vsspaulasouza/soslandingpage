ed263c47ee24a10097d72ddd869da74283c6b527
Vinícius Soares <vsspaulasouza@gmail.com>
2022-04-12 19:14:42 -0300

    fix: added tag figure

--- src/components/about/Slider.jsx
+++ src/components/about/Slider.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Navigation, Pagination, EffectCards, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Screenshot1 from "../../images/screenshot1.png";
-import Screenshot2 from "../../images/screenshot2.png";
-import Screenshot3 from "../../images/screenshot3.png";
+import Screenshot2 from "../../images/screenshot2.jpg";
+import Screenshot3 from "../../images/screenshot3.jpg";
 import Screenshot4 from "../../images/screenshot4.png";
 import "swiper/scss";
 import "./Slider.scss";
@@ -25,16 +25,24 @@ function Slider() {
       width={502}
     >
       <SwiperSlide>
-        <img src={Screenshot1} alt="Screnshot 1" className="slider-img" />
+        <figure>
+          <img src={Screenshot1} alt="Screnshot 1" className="slider-img" />
+        </figure>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={Screenshot2} alt="Screnshot 2" className="slider-img" />
+        <figure>
+          <img src={Screenshot2} alt="Screnshot 2" className="slider-img" />
+        </figure>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={Screenshot3} alt="Screnshot 3" className="slider-img" />
+        <figure>
+          <img src={Screenshot3} alt="Screnshot 3" className="slider-img" />
+        </figure>
       </SwiperSlide>
       <SwiperSlide>
-        <img src={Screenshot4} alt="Screnshot 4" className="slider-img" />
+        <figure>
+          <img src={Screenshot4} alt="Screnshot 4" className="slider-img" />
+        </figure>
       </SwiperSlide>
     </Swiper>
   );
